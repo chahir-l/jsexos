f8ed02328086870ed6670adf11f02a1d210917f9
Artois <artoislabs@gmail.com>
2020-08-26 18:09:36 +0200

    display element tab

---
+++ boucles.js
@@ -0,0 +1,10 @@
+console.log('test')
+
+let fisrtAr = ['hello', 'every', 'one']
+/**
+ * display each element of the array
+ */
+for (i=0; i< fisrtAr.length; i++)
+{
+    console.log(fisrtAr[i] + "\n")
+}
